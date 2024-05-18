72d3411a00c95e0a81ab64ab1b00d9cdf4466192
kleethesama <38240805+kleethesama@users.noreply.github.com>
2024-05-18 12:56:18 +0200

    Add project files.

---
+++ RagnarockWebApp/Data/RagnarockWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppWithDatabase.Models;
+
+namespace RagnarockWebApp.Data
+{
+    public class RagnarockWebAppContext : DbContext
+    {
+        public RagnarockWebAppContext (DbContextOptions<RagnarockWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppWithDatabase.Models.User> User { get; set; } = default!;
+    }
+}
