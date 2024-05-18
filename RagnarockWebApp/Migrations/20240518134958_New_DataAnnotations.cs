fc76002cbd706ae76180594a80a63d9f3188d331
kleethesama <38240805+kleethesama@users.noreply.github.com>
2024-05-18 19:20:02 +0200

    Almost done with database, so need to change its location and implement password hashing.

---
+++ RagnarockWebApp/Migrations/20240518134958_New_DataAnnotations.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RagnarockWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class New_DataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "User",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- RagnarockWebApp/Models/User.cs
+++ RagnarockWebApp/Models/User.cs
@@ -1,10 +1,21 @@
-﻿namespace WebAppWithDatabase.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppWithDatabase.Models
 {
     public class User
     {
         public int Id { get; set; }
+
+        [Required, StringLength(12, MinimumLength = 3), RegularExpression(@"^[a-zA-Z\d]*$")]
         public string? Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
         public string? Password { get; set; }
-        public string? Email { get; set;}
+
+        /* Must figure out how get regex match with a ".*" for the domain (Example: .com)
+         * because right now it doesn't check for a valid email with domain.
+         */ 
+        [Required, DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
     }
 }
