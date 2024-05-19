847a7da72ecea007d79caa686eb8743e48d3cbcf
kleethesama <38240805+kleethesama@users.noreply.github.com>
2024-05-20 01:02:45 +0200

    Unit tested the password hasher. No problems so far.

--- RagnarockWebApp/Pages/Users/Create.cshtml.cs
+++ RagnarockWebApp/Pages/Users/Create.cshtml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using RagnarockWebApp.Data;
 using RagnarockWebApp.Interfaces;
 using WebAppWithDatabase.Models;
--- RagnarockWebApp/Pages/Users/Index.cshtml.cs
+++ RagnarockWebApp/Pages/Users/Index.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RagnarockWebApp.Data;
+using RagnarockWebApp.Models;
 using WebAppWithDatabase.Models;
 
 namespace RagnarockWebApp.Pages.Users
@@ -24,6 +26,8 @@ namespace RagnarockWebApp.Pages.Users
         public async Task OnGetAsync()
         {
             User = await _context.User.ToListAsync();
+            var hasher = new PwdHasher();
+            Debug.Assert(hasher.GetHash("qwe") == User[0].Password);
         }
     }
 }
