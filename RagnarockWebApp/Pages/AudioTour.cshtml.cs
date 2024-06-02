463a81ea99e9beafda917b2509903d0247e5f86d
kleethesama <38240805+kleethesama@users.noreply.github.com>
2024-06-02 15:51:00 +0200

    The audio tour has been given its own page.

---
+++ RagnarockWebApp/Pages/AudioTour.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RagnarockWebApp.Pages
+{
+    public class AudioTourModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
